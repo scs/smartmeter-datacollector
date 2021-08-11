b6772d77b505f7330728d1627c35677b00b27102
Sascha Montellese <sascha.montellese@scs.ch>
2021-08-11 08:33:44 +0000

    add license header to all Python files

--- smartmeter_datacollector/smartmeter/cosem.py
+++ smartmeter_datacollector/smartmeter/cosem.py
@@ -1,3 +1,10 @@
+#
+# Copyright (C) 2021 Supercomputing Systems AG
+# This file is part of smartmeter-datacollector.
+#
+# SPDX-License-Identifier: GPL-2.0-only
+# See LICENSES/README.md for more information.
+#
 from dataclasses import dataclass
 from typing import List
 
