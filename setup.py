b74c964e67faf49294fdfe38dd868c64c46e8362
Sascha Montellese <sascha.montellese@scs.ch>
2021-08-20 12:46:06 +0000

    release v0.3.0

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     author="Supercomputing Systems AG",
     author_email="info@scs.ch",
     classifiers=[
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
