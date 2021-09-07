62725c94b21187f95821c1f0f4a75a31fb4a9a91
Mario Coray <mario.coray@scs.ch>
2021-09-07 10:18:48 +0200

    add licence headers to tests

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2021 Supercomputing Systems AG
+# This file is part of smartmeter-datacollector.
+#
+# SPDX-License-Identifier: GPL-2.0-only
+# See LICENSES/README.md for more information.
+#
