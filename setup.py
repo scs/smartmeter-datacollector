a6c76fe7e18f50f90fc00f6cff97454deddff38c
Mario Coray <mario.coray@scs.ch>
2021-09-09 23:21:38 +0200

    update gurux-dlms to 1.0.107; freeze minor version nr of dependencies

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     install_requires=[
         "aioserial==1.3.0",
         "asyncio-mqtt==0.10.0",
-        "gurux-dlms==1.0.104",
+        "gurux-dlms==1.0.107",
         "paho-mqtt==1.5.1",
         "pyserial==3.5",
     ],
