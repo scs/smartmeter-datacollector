d94d839ba94db90a379abe9e2bbdfb87867b3fd7
Mario Coray <mario.coray@scs.ch>
2021-09-09 22:40:00 +0200

    complete iskra am550 meter; * with yet known COSEM objects * only unencrypted meter supported thus far

--- smartmeter_datacollector/smartmeter/iskraam550.py
+++ smartmeter_datacollector/smartmeter/iskraam550.py
@@ -9,12 +9,62 @@ import logging
 
 import serial
 
-from .cosem import Cosem
+from .cosem import Cosem, RegisterCosem
 from .hdlc_dlms_parser import HdlcDlmsParser
-from .meter import Meter
+from .meter import Meter, MeterError
+from .meter_data import MeterDataPointTypes
+from .reader import ReaderError
 from .serial_reader import SerialConfig, SerialReader
 
 LOGGER = logging.getLogger("smartmeter")
+
+ISKRA_AM550_COSEM_REGISTERS = [
+    RegisterCosem("1.1.1.7.0.255", MeterDataPointTypes.ACTIVE_POWER_P.value),
+    RegisterCosem("1.1.2.7.0.255", MeterDataPointTypes.ACTIVE_POWER_N.value),
+    RegisterCosem("1.1.3.7.0.255", MeterDataPointTypes.REACTIVE_POWER_P.value),
+    RegisterCosem("1.1.4.7.0.255", MeterDataPointTypes.REACTIVE_POWER_N.value),
+
+    RegisterCosem("1.0.13.7.0.255", MeterDataPointTypes.POWER_FACTOR.value, 0.001),
+    RegisterCosem("1.0.14.7.0.255", MeterDataPointTypes.NET_FREQUENCY.value),
+
+    RegisterCosem("1.0.21.7.0.255", MeterDataPointTypes.ACTIVE_POWER_P_L1.value),
+    RegisterCosem("1.0.22.7.0.255", MeterDataPointTypes.ACTIVE_POWER_N_L1.value),
+    RegisterCosem("1.0.23.7.0.255", MeterDataPointTypes.REACTIVE_POWER_P_L1.value),
+    RegisterCosem("1.0.24.7.0.255", MeterDataPointTypes.REACTIVE_POWER_N_L1.value),
+
+    RegisterCosem("1.0.31.7.0.255", MeterDataPointTypes.CURRENT_L1.value),
+    RegisterCosem("1.0.32.7.0.255", MeterDataPointTypes.VOLTAGE_L1.value),
+
+    RegisterCosem("1.0.41.7.0.255", MeterDataPointTypes.ACTIVE_POWER_P_L2.value),
+    RegisterCosem("1.0.42.7.0.255", MeterDataPointTypes.ACTIVE_POWER_N_L2.value),
+    RegisterCosem("1.0.43.7.0.255", MeterDataPointTypes.REACTIVE_POWER_P_L2.value),
+    RegisterCosem("1.0.44.7.0.255", MeterDataPointTypes.REACTIVE_POWER_N_L2.value),
+
+    RegisterCosem("1.0.51.7.0.255", MeterDataPointTypes.CURRENT_L2.value),
+    RegisterCosem("1.0.52.7.0.255", MeterDataPointTypes.VOLTAGE_L2.value),
+
+    RegisterCosem("1.0.61.7.0.255", MeterDataPointTypes.ACTIVE_POWER_P_L3.value),
+    RegisterCosem("1.0.62.7.0.255", MeterDataPointTypes.ACTIVE_POWER_N_L3.value),
+    RegisterCosem("1.0.63.7.0.255", MeterDataPointTypes.REACTIVE_POWER_P_L3.value),
+    RegisterCosem("1.0.64.7.0.255", MeterDataPointTypes.REACTIVE_POWER_N_L3.value),
+
+    RegisterCosem("1.0.71.7.0.255", MeterDataPointTypes.CURRENT_L3.value),
+    RegisterCosem("1.0.72.7.0.255", MeterDataPointTypes.VOLTAGE_L3.value),
+
+    RegisterCosem("1.0.81.7.40.255", MeterDataPointTypes.ANGLE_UI_L1.value),
+    RegisterCosem("1.0.81.7.51.255", MeterDataPointTypes.ANGLE_UI_L2.value),
+    RegisterCosem("1.0.81.7.62.255", MeterDataPointTypes.ANGLE_UI_L3.value),
+
+    RegisterCosem("1.1.1.8.0.255", MeterDataPointTypes.ACTIVE_ENERGY_P.value),
+    RegisterCosem("1.1.2.8.0.255", MeterDataPointTypes.ACTIVE_ENERGY_N.value),
+    RegisterCosem("1.1.3.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_P.value),
+    RegisterCosem("1.1.4.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_N.value),
+
+    RegisterCosem("1.1.5.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_Q1.value),
+    RegisterCosem("1.1.6.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_Q2.value),
+    RegisterCosem("1.1.7.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_Q3.value),
+    RegisterCosem("1.1.8.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_Q4.value),
+]
 
 
 class IskraAM550(Meter):
@@ -31,12 +81,17 @@ class IskraAM550(Meter):
             stop_bits=serial.STOPBITS_ONE,
             termination=IskraAM550.HDLC_FLAG
         )
-        self._serial = SerialReader(serial_config, self._data_received)
+        try:
+            self._serial = SerialReader(serial_config, self._data_received)
+        except ReaderError as ex:
+            LOGGER.fatal("Unable to setup serial reader for Iskra AM550. '%s'", ex)
+            raise MeterError("Failed setting up Iskra AM550.") from ex
         cosem_config = Cosem(
             fallback_id=port,
-            register_obis=[]
+            register_obis=ISKRA_AM550_COSEM_REGISTERS
         )
         self._parser = HdlcDlmsParser(cosem_config)
+        LOGGER.info("Successfully set up Iskra AM550 smart meter on '%s'.", port)
 
     async def start(self) -> None:
         await self._serial.start_and_listen()
@@ -44,10 +99,12 @@ class IskraAM550(Meter):
     def _data_received(self, received_data: bytes) -> None:
         if not received_data:
             return
-        if received_data == self.HDLC_FLAG:
+        if received_data == IskraAM550.HDLC_FLAG:
             self._parser.append_to_hdlc_buffer(received_data)
             return
+
         self._parser.append_to_hdlc_buffer(received_data)
         if self._parser.extract_data_from_hdlc_frames():
             dlms_objects = self._parser.parse_to_dlms_objects()
-            LOGGER.debug(dlms_objects)
+            data_points = self._parser.convert_dlms_bundle_to_reader_data(dlms_objects)
+            self._notify_observers(data_points)
