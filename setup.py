b3a6052d61b72c228eb74faab96999d03db1977a
Sascha Montellese <sascha.montellese@scs.ch>
2021-10-25 12:05:15 +0200

    change development status classifier from Beta to Stable

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     author="Supercomputing Systems AG",
     author_email="info@scs.ch",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
