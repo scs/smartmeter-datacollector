c5148532aa58d93bb40f68414eec1e22fe0c43ae
Mario Coray <mario.coray@scs.ch>
2022-03-31 11:17:24 +0200

    update copyright year to 2022

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (C) 2021 Supercomputing Systems AG
+# Copyright (C) 2022 Supercomputing Systems AG
 # This file is part of smartmeter-datacollector.
 #
 # SPDX-License-Identifier: GPL-2.0-only
--- smartmeter_datacollector/smartmeter/cosem.py
+++ smartmeter_datacollector/smartmeter/cosem.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 Supercomputing Systems AG
+# Copyright (C) 2022 Supercomputing Systems AG
 # This file is part of smartmeter-datacollector.
 #
 # SPDX-License-Identifier: GPL-2.0-only
