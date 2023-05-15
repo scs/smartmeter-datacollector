a1008793bc74022897c4160a9565874a0f0cc9d3
Mario Coray <mario.coray@scs.ch>
2023-05-15 20:41:21 +0200

    integrate OBISCode object into parser and meters

--- smartmeter_datacollector/smartmeter/hdlc_dlms_parser.py
+++ smartmeter_datacollector/smartmeter/hdlc_dlms_parser.py
@@ -79,11 +79,11 @@ class HdlcDlmsParser:
         self._hdlc_buffer.clear()
         return True
 
-    def parse_to_dlms_objects(self) -> Dict[str, GXDLMSObject]:
+    def parse_to_dlms_objects(self) -> List[GXDLMSObject]:
         if not isinstance(self._dlms_data.value, list) or not self._dlms_data.value:
             self._dlms_data.clear()
-            LOGGER.warning("DLMS data is no list or empty list. Not parsable.")
-            return {}
+            LOGGER.error("DLMS data is no list or empty list. Not parsable.")
+            return []
 
         # pylint: disable=unsubscriptable-object
         if isinstance(self._dlms_data.value[0], list):
@@ -91,40 +91,27 @@ class HdlcDlmsParser:
             dlms_objects = self._parse_dlms_with_push_object_list()
         else:
             # message without push-object-list
-            # try to extract OBIS codes if available in message
-            obis_codes = HdlcDlmsParser.extract_obis(self._dlms_data.value)
-            if obis_codes:
-                # message contains OBIS code/value pairs
-                # message format supported: timestamp + OBIS code/value pairs
-                push_setup = GXDLMSPushSetup()
-                push_setup.pushObjectList.append((GXDLMSClock(), GXDLMSCaptureObject(2, 0)))
-                push_setup.pushObjectList.extend((GXDLMSRegister(obis.to_gurux_str()), GXDLMSCaptureObject(2, 0))
-                                                 for obis in obis_codes)
-
-                values = HdlcDlmsParser.extract_values(self._dlms_data.value)
-                if len(values) != len(obis_codes):
-                    LOGGER.warning("Values list and OBIS code list length not equal.")
-
-                for index, (obj, attr_ind) in enumerate(push_setup.pushObjectList):
-                    self._client.updateValue(obj, attr_ind.attributeIndex, values[index])
-                    LOGGER.debug("%d %s %s %s: %s", index, obj.objectType, obj.logicalName,
-                                 attr_ind.attributeIndex, obj.getValues()[attr_ind.attributeIndex - 1])
-                dlms_objects = {obj.getName(): obj for obj, _ in push_setup.pushObjectList}
-            else:
+            dlms_objects = self._parse_dlms_without_push_list()
+            if not dlms_objects:
                 # message contains no OBIS codes, only values, which is not supported.
-                LOGGER.warning("DLMS data is formatted in unknown structure and cannot be parsed.")
-                dlms_objects = {}
-
+                LOGGER.warning("DLMS message is formatted in unknown structure and cannot be parsed by this software.")
         self._dlms_data.clear()
         return dlms_objects
 
-    def convert_dlms_bundle_to_reader_data(self, dlms_objects: Dict[str, GXDLMSObject]) -> List[MeterDataPoint]:
-        meter_id = self._cosem.retrieve_id(dlms_objects)
-        timestamp = self._cosem.retrieve_timestamp(dlms_objects)
+    def convert_dlms_bundle_to_reader_data(self, dlms_objects: List[GXDLMSObject]) -> List[MeterDataPoint]:
+        obis_obj_pairs = {}
+        for obj in dlms_objects:
+            try:
+                obis_obj_pairs[OBISCode.from_string(str(obj.logicalName))] = obj
+            except ValueError as ex:
+                LOGGER.warning("Skipping unparsable DLMS object. (Reason: %s)", ex)
+
+        meter_id = self._cosem.retrieve_id(obis_obj_pairs)
+        timestamp = self._cosem.retrieve_timestamp(obis_obj_pairs)
 
         # Extract register data
         data_points: List[MeterDataPoint] = []
-        for obis, obj in filter(lambda o: o[1].getObjectType() == ObjectType.REGISTER, dlms_objects.items()):
+        for obis, obj in filter(lambda o: o[1].getObjectType() == ObjectType.REGISTER, obis_obj_pairs.items()):
             reg_type = self._cosem.get_register(obis)
             if reg_type and isinstance(obj, GXDLMSRegister):
                 raw_value = self._extract_register_value(obj)
@@ -140,13 +127,40 @@ class HdlcDlmsParser:
                 data_points.append(MeterDataPoint(data_point_type, value, meter_id, timestamp))
         return data_points
 
-    def _parse_dlms_with_push_object_list(self) -> Dict[str, GXDLMSObject]:
+    def _parse_dlms_with_push_object_list(self) -> List[GXDLMSObject]:
         parsed_objects: List[Tuple[GXDLMSObject, int]] = []
         parsed_objects = self._client.parsePushObjects(self._dlms_data.value[0])
         for index, (obj, attr_ind) in enumerate(parsed_objects[1:], start=1):
             self._client.updateValue(obj, attr_ind, self._dlms_data.value[index])
             LOGGER.debug("%s %s %s: %s", obj.objectType, obj.logicalName, attr_ind, obj.getValues()[attr_ind - 1])
-        return {obj.getName(): obj for obj, _ in parsed_objects}
+        
+        return [obj for obj, _ in parsed_objects]
+
+    def _parse_dlms_without_push_list(self) -> List[GXDLMSObject]:
+        """Tries to extract OBIS codes and values from message.
+        Supported message format:
+            - timestamp
+            - obis code 1
+            - value 1
+            - obis code 2
+            - value 2
+            - ...
+        """
+        obis_codes, values = HdlcDlmsParser.extract_obis_and_values(self._dlms_data.value)
+        if not obis_codes:
+            return []
+
+        push_setup = GXDLMSPushSetup()
+        if len(values) > len(obis_codes):
+            push_setup.pushObjectList.append((GXDLMSClock(), GXDLMSCaptureObject(2, 0)))
+        push_setup.pushObjectList.extend((GXDLMSRegister(obis.to_gurux_str()), GXDLMSCaptureObject(2, 0))
+                                         for obis in obis_codes)
+
+        for index, (obj, attr_ind) in enumerate(push_setup.pushObjectList):
+            self._client.updateValue(obj, attr_ind.attributeIndex, values[index])
+            LOGGER.debug("%d %s %s %s: %s", index, obj.objectType, obj.logicalName,
+                         attr_ind.attributeIndex, obj.getValues()[attr_ind.attributeIndex - 1])
+        return [obj for obj, _ in push_setup.pushObjectList]
 
     @staticmethod
     def _extract_value_from_data_object(data_object: GXDLMSData) -> Optional[Any]:
@@ -157,16 +171,32 @@ class HdlcDlmsParser:
         return register.getValues()[1]
 
     @staticmethod
-    def extract_obis(data: List[Any]) -> List[OBISCode]:
-        extracted_obis = []
-        for obis_bytes in filter(lambda d: isinstance(d, (bytearray, bytes)), data):
-            obis = OBISCode.from_bytes(obis_bytes)
-            if obis:
-                extracted_obis.append(obis)
-        return extracted_obis
+    def extract_obis_and_values(data: List[Any]) -> Tuple[List[OBISCode], List[Any]]:
+        obis_it = filter(lambda d: isinstance(d[1], (bytearray, bytes)) and OBISCode.is_obis(d[1]), enumerate(data))
+
+        obis_codes = []
+        values = []
+
+        for idx, obis_bytes in obis_it:
+            if idx == 1:
+                # first element (index 0) assumed to be message timestamp
+                values.append(data[0])
+
+            if idx + 1 < len(data) and HdlcDlmsParser.is_value(data[idx + 1]):
+                obis_codes.append(OBISCode.from_bytes(obis_bytes))
+                values.append(data[idx + 1])
+            else:
+                LOGGER.warning("OBIS code without following value skipped.")
+
+        return obis_codes, values
+
+    @staticmethod
+    def is_value(d) -> bool:
+        return isinstance(d, int) or (isinstance(d, (bytearray, bytes)) and not OBISCode.is_obis(d))
 
     @staticmethod
     def extract_values(data: List[Any]) -> List[Any]:
-        return list(filter(
-            lambda d: isinstance(d, int) or (isinstance(d, (bytes, bytearray)) and not OBISCode.is_obis(d)),
-            data))
+        first_obis = next(
+            (i for i, d in enumerate(data) if isinstance(d, (bytearray, bytes)) and OBISCode.is_obis(d))
+        )
+        return data[first_obis+1::2]
--- smartmeter_datacollector/smartmeter/iskraam550.py
+++ smartmeter_datacollector/smartmeter/iskraam550.py
@@ -10,65 +10,12 @@ from typing import Optional
 
 import serial
 
-from .cosem import Cosem, RegisterCosem
+from .cosem import Cosem
 from .meter import MeterError, SerialHdlcDlmsMeter
-from .meter_data import MeterDataPointTypes
 from .reader import ReaderError
 from .serial_reader import SerialConfig
 
 LOGGER = logging.getLogger("smartmeter")
-
-ISKRA_AM550_COSEM_REGISTERS = [
-    RegisterCosem("1.1.1.7.0.255", MeterDataPointTypes.ACTIVE_POWER_P.value),
-    RegisterCosem("1.1.2.7.0.255", MeterDataPointTypes.ACTIVE_POWER_N.value),
-    RegisterCosem("1.1.3.7.0.255", MeterDataPointTypes.REACTIVE_POWER_P.value),
-    RegisterCosem("1.1.4.7.0.255", MeterDataPointTypes.REACTIVE_POWER_N.value),
-
-    RegisterCosem("1.0.13.7.0.255", MeterDataPointTypes.POWER_FACTOR.value, 0.001),
-    RegisterCosem("1.0.14.7.0.255", MeterDataPointTypes.NET_FREQUENCY.value),
-
-    RegisterCosem("1.0.21.7.0.255", MeterDataPointTypes.ACTIVE_POWER_P_L1.value),
-    RegisterCosem("1.0.22.7.0.255", MeterDataPointTypes.ACTIVE_POWER_N_L1.value),
-    RegisterCosem("1.0.23.7.0.255", MeterDataPointTypes.REACTIVE_POWER_P_L1.value),
-    RegisterCosem("1.0.24.7.0.255", MeterDataPointTypes.REACTIVE_POWER_N_L1.value),
-
-    RegisterCosem("1.0.31.7.0.255", MeterDataPointTypes.CURRENT_L1.value),
-    RegisterCosem("1.0.32.7.0.255", MeterDataPointTypes.VOLTAGE_L1.value),
-
-    RegisterCosem("1.0.41.7.0.255", MeterDataPointTypes.ACTIVE_POWER_P_L2.value),
-    RegisterCosem("1.0.42.7.0.255", MeterDataPointTypes.ACTIVE_POWER_N_L2.value),
-    RegisterCosem("1.0.43.7.0.255", MeterDataPointTypes.REACTIVE_POWER_P_L2.value),
-    RegisterCosem("1.0.44.7.0.255", MeterDataPointTypes.REACTIVE_POWER_N_L2.value),
-
-    RegisterCosem("1.0.51.7.0.255", MeterDataPointTypes.CURRENT_L2.value),
-    RegisterCosem("1.0.52.7.0.255", MeterDataPointTypes.VOLTAGE_L2.value),
-
-    RegisterCosem("1.0.61.7.0.255", MeterDataPointTypes.ACTIVE_POWER_P_L3.value),
-    RegisterCosem("1.0.62.7.0.255", MeterDataPointTypes.ACTIVE_POWER_N_L3.value),
-    RegisterCosem("1.0.63.7.0.255", MeterDataPointTypes.REACTIVE_POWER_P_L3.value),
-    RegisterCosem("1.0.64.7.0.255", MeterDataPointTypes.REACTIVE_POWER_N_L3.value),
-
-    RegisterCosem("1.0.71.7.0.255", MeterDataPointTypes.CURRENT_L3.value),
-    RegisterCosem("1.0.72.7.0.255", MeterDataPointTypes.VOLTAGE_L3.value),
-
-    RegisterCosem("1.0.81.7.40.255", MeterDataPointTypes.ANGLE_UI_L1.value),
-    RegisterCosem("1.0.81.7.51.255", MeterDataPointTypes.ANGLE_UI_L2.value),
-    RegisterCosem("1.0.81.7.62.255", MeterDataPointTypes.ANGLE_UI_L3.value),
-
-    RegisterCosem("1.1.1.8.0.255", MeterDataPointTypes.ACTIVE_ENERGY_P.value),
-    RegisterCosem("1.1.1.8.1.255", MeterDataPointTypes.ACTIVE_ENERGY_P_T1.value),
-    RegisterCosem("1.1.1.8.2.255", MeterDataPointTypes.ACTIVE_ENERGY_P_T2.value),
-    RegisterCosem("1.1.2.8.0.255", MeterDataPointTypes.ACTIVE_ENERGY_N.value),
-    RegisterCosem("1.1.2.8.1.255", MeterDataPointTypes.ACTIVE_ENERGY_N_T1.value),
-    RegisterCosem("1.1.2.8.2.255", MeterDataPointTypes.ACTIVE_ENERGY_N_T2.value),
-    RegisterCosem("1.1.3.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_P.value),
-    RegisterCosem("1.1.4.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_N.value),
-
-    RegisterCosem("1.1.5.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_Q1.value),
-    RegisterCosem("1.1.6.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_Q2.value),
-    RegisterCosem("1.1.7.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_Q3.value),
-    RegisterCosem("1.1.8.8.0.255", MeterDataPointTypes.REACTIVE_ENERGY_Q4.value),
-]
 
 
 class IskraAM550(SerialHdlcDlmsMeter):
@@ -81,10 +28,7 @@ class IskraAM550(SerialHdlcDlmsMeter):
             stop_bits=serial.STOPBITS_ONE,
             termination=IskraAM550.HDLC_FLAG
         )
-        cosem = Cosem(
-            fallback_id=port,
-            register_obis=ISKRA_AM550_COSEM_REGISTERS
-        )
+        cosem = Cosem(fallback_id=port)
         if decryption_key:
             LOGGER.warning("Using the Iskra AM550 meter with encrypted data has NOT BEEN TESTED yet!")
         try:
