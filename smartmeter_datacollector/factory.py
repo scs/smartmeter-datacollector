4a56a1ccae662702531e75c7cdb4aa7b1c3643fd
Mario Coray <mario.coray@scs.ch>
2024-03-13 10:11:22 +0100

    add L+G E570 smart meter

--- smartmeter_datacollector/factory.py
+++ smartmeter_datacollector/factory.py
@@ -18,6 +18,7 @@ from .smartmeter.iskraam550 import IskraAM550
 from .smartmeter.kamstrup_han import KamstrupHAN
 from .smartmeter.lge360 import LGE360
 from .smartmeter.lge450 import LGE450
+from .smartmeter.lge570 import LGE570
 from .smartmeter.meter import Meter, MeterError
 
 
@@ -31,6 +32,13 @@ def build_meters(config: ConfigParser) -> List[Meter]:
                 meters.append(LGE450(
                     port=meter_config.get('port', "/dev/ttyUSB0"),
                     baudrate=meter_config.getint('baudrate', LGE450.BAUDRATE),
+                    decryption_key=meter_config.get('key'),
+                    use_system_time=meter_config.getboolean('systemtime', False)
+                ))
+            elif meter_type == "lge570":
+                meters.append(LGE570(
+                    port=meter_config.get('port', "/dev/ttyUSB0"),
+                    baudrate=meter_config.getint('baudrate', LGE570.BAUDRATE),
                     decryption_key=meter_config.get('key'),
                     use_system_time=meter_config.getboolean('systemtime', False)
                 ))
