297ef5bf68d8c92ac3a5559dadad57b9af84b671
Mario Coray <mario.coray@scs.ch>
2026-05-27 15:48:38 +0200

    add L+G E450 test with new meter config

--- tests/test_lge450.py
+++ tests/test_lge450.py
@@ -11,7 +11,7 @@ from pytest_mock.plugin import MockerFixture
 from smartmeter_datacollector.smartmeter.lge450 import LGE450
 from smartmeter_datacollector.smartmeter.meter_data import MeterDataBundle, MeterDataPointTypes
 from tests.conftest import split_hex_data_to_frames
-from tests.testdata.lg_e450 import UNENCRYPTED_VALID_DATA
+from tests.testdata.lg_e450 import UNENCRYPTED_VALID_DATA, UNENCRYPTED_VALID_DATA_CFG2026
 
 
 @pytest.mark.asyncio
@@ -86,3 +86,46 @@ async def test_lge450_do_not_provide_invalid_data(mocker: MockerFixture):
 
     serial_mock.start_and_listen.assert_awaited_once()
     observer.notify.assert_not_called()
+
+
+@pytest.mark.asyncio
+async def test_lge450_vse_standard(mocker: MockerFixture):
+    observer = mocker.stub("collector_mock")
+    observer.mock_add_spec(['notify'])
+    serial_mock = mocker.patch("smartmeter_datacollector.smartmeter.meter.SerialReader", autospec=True).return_value
+    meter = LGE450("/test/port")
+    meter.register(observer)
+
+    frames = split_hex_data_to_frames(UNENCRYPTED_VALID_DATA_CFG2026)
+
+    def data_received():
+        for frame in frames:
+            meter._data_received(frame)
+
+    serial_mock.start_and_listen.side_effect = data_received
+
+    await meter.start()
+
+    serial_mock.start_and_listen.assert_awaited_once()
+    observer.notify.assert_called_once()
+    data_bundle = observer.notify.call_args.args[0]
+    assert isinstance(data_bundle, MeterDataBundle)
+    values = data_bundle.data_points
+
+    def point_value(point_type: MeterDataPointTypes):
+        return next(data.value for data in values if data.type == point_type.value)
+
+    assert point_value(MeterDataPointTypes.ACTIVE_POWER_P) == 28
+    assert point_value(MeterDataPointTypes.ACTIVE_POWER_N) == 0
+    assert point_value(MeterDataPointTypes.ACTIVE_ENERGY_P) == 482939
+    assert point_value(MeterDataPointTypes.ACTIVE_ENERGY_N) == 0
+    assert point_value(MeterDataPointTypes.REACTIVE_ENERGY_P) == 37
+    assert point_value(MeterDataPointTypes.REACTIVE_ENERGY_N) == 344697
+    assert point_value(MeterDataPointTypes.VOLTAGE_L1) == 236
+    assert point_value(MeterDataPointTypes.VOLTAGE_L2) == 236
+    assert point_value(MeterDataPointTypes.VOLTAGE_L3) == 0
+    assert point_value(MeterDataPointTypes.CURRENT_L1) == 0.09
+    assert point_value(MeterDataPointTypes.CURRENT_L2) == 0.08
+    assert point_value(MeterDataPointTypes.CURRENT_L3) == 0.00
+    assert data_bundle.source == "LGZ1030653520967"
+    assert data_bundle.timestamp.strftime(r"%d.%m.%Y %H:%M:%S") == "04.05.2026 09:39:21"
